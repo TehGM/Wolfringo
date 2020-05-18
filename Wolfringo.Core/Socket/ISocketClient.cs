7674d8ac8aa59c5752dfd724b098b42cfe576002
TehGM <wojop95@gmail.com>
2020-05-18 19:34:47 +0100

    Custom socket client template

---
+++ Wolfringo.Core/Socket/ISocketClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TehGM.Wolfringo.Socket
+{
+    public interface ISocketClient
+    {
+        Task ConnectAsync(Uri url, CancellationToken cancellationToken = default);
+        Task DisconnectAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Wolfringo.Core/Socket/SocketClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TehGM.Wolfringo.Socket
+{
+    public class SocketClient : ISocketClient, IDisposable
+    {
+        private readonly ClientWebSocket _websocketClient;
+        private CancellationTokenSource _connectionCts;
+
+        public async Task ConnectAsync(Uri url, CancellationToken cancellationToken = default)
+        {
+            _connectionCts?.Cancel();
+            _connectionCts?.Dispose();
+            _connectionCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+            await _websocketClient.ConnectAsync(url, _connectionCts.Token).ConfigureAwait(false);
+            _ = ConnectionTaskAsync();
+        }
+
+        public Task DisconnectAsync(CancellationToken cancellationToken = default)
+        {
+            _connectionCts?.Cancel();
+            _connectionCts?.Dispose();
+            return _websocketClient.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closed by client", cancellationToken);
+        }
+
+        private async Task ConnectionTaskAsync(CancellationToken cancellationToken = default)
+        {
+            ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024 * 16]);
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                // TODO: implement message reading
+            }
+        }
+
+        public void Dispose()
+        {
+            this.DisconnectAsync().GetAwaiter().GetResult();
+        }
+    }
+}
