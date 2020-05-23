9e957f053709a14ad8e211c603105ab65efff8cc
TehGM <wojop95@gmail.com>
2020-05-23 20:10:23 +0100

    Added default message response serializer

---
+++ Wolfringo.Core/Messages/Serialization/IMessageResponseSerializer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using TehGM.Wolfringo.Messages.Responses;
+
+namespace TehGM.Wolfringo.Messages.Serialization
+{
+    public interface IMessageResponseSerializer
+    {
+        WolfResponse Deserialize(Type responseType, SerializedMessageData responseData);
+    }
+}
