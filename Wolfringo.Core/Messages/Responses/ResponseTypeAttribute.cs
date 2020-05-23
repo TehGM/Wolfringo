46ed3f48f891c0b765cf47b60f0c097e2792451d
TehGM <wojop95@gmail.com>
2020-05-23 19:22:05 +0100

    Added response type attribute

---
+++ Wolfringo.Core/Messages/Responses/ResponseTypeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TehGM.Wolfringo.Messages.Responses;
+
+namespace TehGM.Wolfringo.Messages.Responses
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
+    public class ResponseTypeAttribute : Attribute
+    {
+        public static readonly Type ParentResponseType = typeof(WolfResponse);
+        public Type ResponseType { get; }
+
+        public ResponseTypeAttribute(Type responseType)
+            : base()
+        {
+            if (!ParentResponseType.IsAssignableFrom(responseType))
+                throw new ArgumentException($"Response type must inherit from {ParentResponseType.FullName}", nameof(responseType));
+
+            this.ResponseType = responseType;
+        }
+    }
+}
