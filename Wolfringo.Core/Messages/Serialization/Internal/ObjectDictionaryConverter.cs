acc9ae6ffc61a565d21bd28cb3442d0e0a7e7606
TehGM <wojop95@gmail.com>
2020-05-31 14:42:09 +0100

    Added custom object dictionary converter

---
+++ Wolfringo.Core/Messages/Serialization/Internal/ObjectDictionaryConverter.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace TehGM.Wolfringo.Messages.Serialization.Internal
+{
+    public class ObjectDictionaryConverter<TKey, TValue> : JsonConverter
+    {
+        private readonly string _keyPropName;
+
+        public ObjectDictionaryConverter(string keyPropertyName)
+        {
+            this._keyPropName = keyPropertyName;
+        }
+
+        public ObjectDictionaryConverter()
+            : this("Key") { }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(IEnumerable<KeyValuePair<TKey, TValue>>).IsAssignableFrom(objectType);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            IEnumerable<KeyValuePair<TKey, TValue>> collection = (IEnumerable<KeyValuePair<TKey, TValue>>)value;
+            JArray results = new JArray();
+            foreach (KeyValuePair<TKey, TValue> pair in collection)
+            {
+                JObject item = JObject.FromObject(pair.Value, serializer);
+                item.Add(_keyPropName, JToken.FromObject(pair.Key, serializer));
+                results.Add(item);
+            }
+            results.WriteTo(writer);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JArray jsonArray = JArray.Load(reader);
+            Dictionary<TKey, TValue> results = new Dictionary<TKey, TValue>(jsonArray.Count);
+            foreach (JToken obj in jsonArray)
+            {
+                TKey key = obj[_keyPropName].ToObject<TKey>(serializer);
+                TValue value = obj.ToObject<TValue>(serializer);
+                results.Add(key, value);
+            }
+            return results;
+        }
+    }
+}
