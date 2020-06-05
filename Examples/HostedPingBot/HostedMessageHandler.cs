fa325eab621e33ff0836987ce33d4b9679e3b1c3
TehGM <wojop95@gmail.com>
2020-06-05 14:28:20 +0100

    Added example hosted client

---
+++ Examples/HostedPingBot/HostedMessageHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+using TehGM.Wolfringo.Hosting;
+using TehGM.Wolfringo.Messages;
+
+namespace TehGM.Wolfringo.Examples.HostedPingBot
+{
+    public class HostedMessageHandler : IHostedService
+    {
+        private readonly IHostedWolfClient _client;
+
+        // can also be IWolfClient
+        public HostedMessageHandler(IHostedWolfClient client)
+        {
+            this._client = client;
+            this._client.AddMessageListener<ChatMessage>(OnChatMessage);
+        }
+
+        private async void OnChatMessage(ChatMessage message)
+        {
+            if (message.IsPrivateMessage)
+                await _client.SendAsync(ChatMessage.TextMessage(message.SenderID.Value, message.IsGroupMessage, "Hello there!"));
+        }
+
+        Task IHostedService.StartAsync(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+
+        Task IHostedService.StopAsync(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+    }
+}
---
+++ Examples/HostedPingBot/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using TehGM.Wolfringo.Hosting;
+
+namespace TehGM.Wolfringo.Examples.HostedPingBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // this is an example showing configuration for .NET Core 3.0+
+            // for host configuration instructions in .NET Core 2.1 and 2.2, 
+            // see https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-2.1
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddJsonFile("appsecrets-example.json", optional: true);
+                    config.AddJsonFile("appsecrets.json", optional: true);
+                    config.AddJsonFile($"appsecrets.{context.HostingEnvironment.EnvironmentName}.json", optional: true);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.Configure<HostedWolfClientOptions>(context.Configuration.GetSection("WolfClient"));
+                    services.AddHostedService<HostedMessageHandler>();
+                    services.AddWolfClient();
+                })
+                .ConfigureLogging((context, config) =>
+                {
+                    config.SetMinimumLevel(LogLevel.Debug);
+                })
+                .Build();
+            await host.RunAsync();
+        }
+    }
+}
