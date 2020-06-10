e8a32dd94572788a7bf8560d31099d8bf7a87c47
TehGM <wojop95@gmail.com>
2020-06-10 22:12:49 +0100

    Don't manually throw WS exception at all

--- Wolfringo.Core/Socket/SocketClient.cs
+++ Wolfringo.Core/Socket/SocketClient.cs
@@ -158,7 +158,6 @@ namespace TehGM.Wolfringo.Socket
             }
             catch (TaskCanceledException) { }
             catch (OperationCanceledException) { }
-            catch (WebSocketException ex) when (ex.WebSocketErrorCode == WebSocketError.ConnectionClosedPrematurely) { }
             catch (Exception ex)
             {
                 ErrorRaised?.Invoke(this, new UnhandledExceptionEventArgs(ex, true));
@@ -194,11 +193,7 @@ namespace TehGM.Wolfringo.Socket
             WebSocketReceiveResult result = await _websocketClient.ReceiveAsync(buffer, cancellationToken).ConfigureAwait(false);
             // cancel further execution if connection was closed
             if (result.MessageType == WebSocketMessageType.Close)
-            {
-                if (result.CloseStatus != WebSocketCloseStatus.NormalClosure)
-                    throw new WebSocketException(WebSocketError.ConnectionClosedPrematurely, result.CloseStatusDescription);
                 return null;
-            }
 
             byte[] contents = null;
             int bytesRead = 0;
