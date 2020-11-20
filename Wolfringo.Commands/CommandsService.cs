64224e92b5155d4c66745a0448a5917b7cc2cf99
TehGM <wojop95@gmail.com>
2020-11-20 18:35:11 +0100

    Improved logs a little bit

--- Wolfringo.Commands/CommandsService.cs
+++ Wolfringo.Commands/CommandsService.cs
@@ -90,11 +90,12 @@ namespace TehGM.Wolfringo.Commands
 
         private void AddAssembly(Assembly assembly)
         {
+            _log?.LogTrace("Loading assembly {Name}", assembly.FullName);
             IEnumerable<TypeInfo> types = assembly.DefinedTypes.Where(t => !t.IsAbstract && !t.ContainsGenericParameters
                 && !Attribute.IsDefined(t, typeof(CompilerGeneratedAttribute)) && Attribute.IsDefined(t, typeof(CommandHandlerAttribute), true));
             if (!types.Any())
             {
-                _log?.LogWarning("Cannot initialize commands from assembly {AssemblyName} - no non-static non-abstract non-generic classes with {Attribute}", assembly.FullName, nameof(CommandHandlerAttribute));
+                _log?.LogWarning("Cannot initialize commands from assembly {Name} - no non-static non-abstract non-generic classes with {Attribute}", assembly.FullName, nameof(CommandHandlerAttribute));
                 return;
             }
             foreach (TypeInfo type in types)
@@ -103,10 +104,11 @@ namespace TehGM.Wolfringo.Commands
 
         private void AddType(TypeInfo type)
         {
+            _log?.LogTrace("Loading handler {Handler}", type.FullName);
             IEnumerable<MethodInfo> methods = type.DeclaredMethods.Where(m => !m.IsStatic && !Attribute.IsDefined(m, typeof(CompilerGeneratedAttribute)) && Attribute.IsDefined(m, typeof(CommandAttributeBase), true));
             if (!methods.Any())
             {
-                _log?.LogWarning("Cannot initialize commands from type {TypeName} - no method with {Attribute}", type.FullName, nameof(CommandAttributeBase));
+                _log?.LogWarning("Cannot initialize commands from type {Handler} - no method with {Attribute}", type.FullName, nameof(CommandAttributeBase));
                 return;
             }
             foreach (MethodInfo method in methods)
@@ -115,10 +117,12 @@ namespace TehGM.Wolfringo.Commands
 
         private void AddMethod(MethodInfo method)
         {
+            _log?.LogTrace("Loading command {Name}", method.Name);
+            Type handlerType = method.DeclaringType;
             IEnumerable<CommandAttributeBase> attributes = method.GetCustomAttributes<CommandAttributeBase>(true);
             if (!attributes.Any())
             {
-                _log?.LogWarning("Cannot initialize command from {TypeName}'s method {MethodName} - {Attribute} missing", method.DeclaringType.FullName, method.Name, nameof(CommandAttributeBase));
+                _log?.LogWarning("Cannot initialize command from {Handler}'s method {Name} - {Attribute} missing", handlerType.FullName, method.Name, nameof(CommandAttributeBase));
                 return;
             }
             foreach (CommandAttributeBase attribute in attributes)
@@ -131,10 +135,14 @@ namespace TehGM.Wolfringo.Commands
                 // check if handler is meant to be pre-initialized. If so, request it from provider to pre-initialize
                 CommandHandlerAttribute handlerAttribute = method.DeclaringType.GetCustomAttribute<CommandHandlerAttribute>(true);
                 if (handlerAttribute?.PreInitialize == true)
+                {
+                    _log?.LogDebug("Pre-initializing command handler {Handler}", handlerType.Name);
                     _handlerProvider.GetCommandHandler(attribute, method.DeclaringType);
+                }
 
                 // add the command
                 _commands.Add((attribute, method));
+                _log?.LogTrace("Command {Name} from handler {Handler} loaded", method.Name, handlerType.Name);
             }
         }
 
@@ -164,17 +172,18 @@ namespace TehGM.Wolfringo.Commands
                         ICommandResult checkResult = await instance.CheckShouldRunAsync(context, this.CancellationToken).ConfigureAwait(false);
                         if (!checkResult.IsSuccess)
                             continue;
+                        _log?.LogTrace("Executing command {Name}", method.Name);
                         ICommandResult executeResult = await instance.ExecuteAsync(context, _services, checkResult, this.CancellationToken).ConfigureAwait(false);
                         if (!executeResult.IsSuccess)
-                            _log?.LogError("Execution of command {MethodName} has failed", method.Name);
+                            _log?.LogError("Execution of command {Name} has failed", method.Name);
                         break;
                     }
                     catch (OperationCanceledException)
                     {
-                        _log?.LogWarning("Execution of command {MethodName} was cancelled", method.Name);
+                        _log?.LogWarning("Execution of command {Name} was cancelled", method.Name);
                         return;
                     }
-                    catch (Exception ex) when (ex.LogAsError(_log, "Unhandled Exception when executing command {MethodName}", method.Name)) { return; }
+                    catch (Exception ex) when (ex.LogAsError(_log, "Unhandled Exception when executing command {Name}", method.Name)) { return; }
                 }
             }
         }
