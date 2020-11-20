b26e52f1adad1936c06e362feb388afd1bd64fc8
TehGM <wojop95@gmail.com>
2020-11-20 17:26:15 +0100

    Added command initializer mapping

---
+++ Wolfringo.Commands/Initialization/DefaultCommandInitializerMap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TehGM.Wolfringo.Commands.Initialization
+{
+    public class DefaultCommandInitializerMap : ICommandInitializerMap
+    {
+        private IDictionary<Type, ICommandInitializer> _map;
+
+        /// <summary>Creates default message serializer map.</summary>
+        /// <param name="fallbackSerializer">Serializer to use as fallback. If null, 
+        /// <see cref="DefaultMessageSerializer{T}"/> for <see cref="IWolfMessage"/> will be used.</param>
+        public DefaultCommandInitializerMap()
+        {
+            this._map = new Dictionary<Type, ICommandInitializer>()
+            {
+                { typeof(RegexCommandAttribute), new RegexCommandInitializer() }
+            };
+        }
+
+        /// <summary>Creates default message serializer map.</summary>
+        /// <param name="additionalMappings">Additional mappings. Can overwrite default mappings.</param>
+        /// <param name="fallbackSerializer">Serializer to use as fallback. If null, 
+        /// <see cref="DefaultMessageSerializer{T}"/> for <see cref="IWolfMessage"/> will be used.</param>
+        public DefaultCommandInitializerMap(IEnumerable<KeyValuePair<Type, ICommandInitializer>> additionalMappings) : this()
+        {
+            foreach (var pair in additionalMappings)
+                this.MapInitializer(pair.Key, pair.Value);
+        }
+
+        public ICommandInitializer GetMappedInitializer(Type commandAttributeType)
+        {
+            this._map.TryGetValue(commandAttributeType, out ICommandInitializer result);
+            return result;
+        }
+
+        public void MapInitializer(Type commandAttributeType, ICommandInitializer initializer)
+            => this._map[commandAttributeType] = initializer;
+    }
+}
---
+++ Wolfringo.Commands/Initialization/ICommandInitializerMap.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TehGM.Wolfringo.Commands.Initialization
+{
+    public interface ICommandInitializerMap
+    {
+        ICommandInitializer GetMappedInitializer(Type commandAttributeType);
+        void MapInitializer(Type commandAttributeType, ICommandInitializer initializer);
+    }
+}
