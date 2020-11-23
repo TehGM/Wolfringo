870766480fc5666663b1681807906c906e246b57
TehGM <wojop95@gmail.com>
2020-11-23 21:59:03 +0100

    Requirements that check if the command is group/private

---
+++ Wolfringo.Commands/Attributes/Requirements/GroupOnlyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TehGM.Wolfringo.Commands
+{
+    /// <summary>Command requirement that checks if message is a group message.</summary>
+    /// <seealso cref="PrivateOnlyAttribute"/>
+    public class GroupOnlyAttribute : CommandRequirementAttribute
+    {
+        /// <inheritdoc/>
+        public override Task<bool> RunAsync(ICommandContext context, CancellationToken cancellationToken = default)
+            => Task.FromResult<bool>(context.Message.IsGroupMessage);
+    }
+}
---
+++ Wolfringo.Commands/Attributes/Requirements/PrivateOnlyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TehGM.Wolfringo.Commands
+{
+    /// <summary>Command requirement that checks if message is a private message.</summary>
+    /// <seealso cref="GroupOnlyAttribute"/>
+    public class PrivateOnlyAttribute : CommandRequirementAttribute
+    {
+        /// <inheritdoc/>
+        public override Task<bool> RunAsync(ICommandContext context, CancellationToken cancellationToken = default)
+            => Task.FromResult<bool>(!context.Message.IsGroupMessage);
+    }
+}
