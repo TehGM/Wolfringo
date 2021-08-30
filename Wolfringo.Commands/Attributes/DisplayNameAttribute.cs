9bb347fc8864a3ecbf1287d200040c1b701f4cd8
TehGM <wojop95@gmail.com>
2021-08-30 17:07:47 +0100

    Added display name attribute

---
+++ Wolfringo.Commands/Attributes/DisplayNameAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TehGM.Wolfringo.Commands
+{
+    /// <summary>Sets display name for the command in the help list.</summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class DisplayNameAttribute : Attribute
+    {
+        /// <summary>Display Name value.</summary>
+        public string Text { get; }
+
+        /// <summary>Sets display name for the command in the help list.</summary>
+        /// <param name="text">Display Name value.</param>
+        public DisplayNameAttribute(string text)
+        {
+            this.Text = text;
+        }
+    }
+}
