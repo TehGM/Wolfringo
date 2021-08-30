49d228b60a67d938534dcae5430846e339299262
TehGM <wojop95@gmail.com>
2021-08-30 17:09:52 +0100

    Added summary attribute

---
+++ Wolfringo.Commands/Attributes/SummaryAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TehGM.Wolfringo.Commands
+{
+    /// <summary>Sets the summary description for the command in the help list.</summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class SummaryAttribute : Attribute
+    {
+        /// <summary>Summary value.</summary>
+        public string Text { get; }
+
+        /// <summary>Sets the summary description for the command in the help list.</summary>
+        /// <param name="text">Summary value.</param>
+        public SummaryAttribute(string text)
+        {
+            this.Text = text;
+        }
+    }
+}
