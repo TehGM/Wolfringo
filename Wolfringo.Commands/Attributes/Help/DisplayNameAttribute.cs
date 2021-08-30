45177e5a719f8a879cd6359afe33ed935c2bfc84
TehGM <wojop95@gmail.com>
2021-08-30 18:01:44 +0100

    Checks and overrides for help attributes

--- Wolfringo.Commands/Attributes/Help/DisplayNameAttribute.cs
+++ Wolfringo.Commands/Attributes/Help/DisplayNameAttribute.cs
@@ -4,7 +4,7 @@ namespace TehGM.Wolfringo.Commands
 {
     /// <summary>Sets display name for the command in the help list.</summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
-    public class DisplayNameAttribute : Attribute
+    public class DisplayNameAttribute : Attribute, IEquatable<DisplayNameAttribute>
     {
         /// <summary>Display Name value.</summary>
         public string Text { get; }
@@ -13,7 +13,22 @@ namespace TehGM.Wolfringo.Commands
         /// <param name="text">Display Name value.</param>
         public DisplayNameAttribute(string text)
         {
+            if (string.IsNullOrWhiteSpace(text))
+                throw new ArgumentNullException(nameof(text), "Display name cannot be null, blank or whitespace");
+
             this.Text = text;
         }
+
+        /// <inheritdoc/>
+        public bool Equals(DisplayNameAttribute other)
+            => other != null && this.Text.Equals(other.Text, StringComparison.Ordinal);
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+            => this.Equals(obj as DisplayNameAttribute);
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+            => this.Text.GetHashCode();
     }
 }
--- Wolfringo.Commands/Attributes/Help/SummaryAttribute.cs
+++ Wolfringo.Commands/Attributes/Help/SummaryAttribute.cs
@@ -4,7 +4,7 @@ namespace TehGM.Wolfringo.Commands
 {
     /// <summary>Sets the summary description for the command in the help list.</summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
-    public class SummaryAttribute : Attribute
+    public class SummaryAttribute : Attribute, IEquatable<SummaryAttribute>
     {
         /// <summary>Summary value.</summary>
         public string Text { get; }
@@ -13,7 +13,22 @@ namespace TehGM.Wolfringo.Commands
         /// <param name="text">Summary value.</param>
         public SummaryAttribute(string text)
         {
+            if (string.IsNullOrWhiteSpace(text))
+                throw new ArgumentNullException(nameof(text), "Summary cannot be null, blank or whitespace");
+
             this.Text = text;
         }
+
+        /// <inheritdoc/>
+        public bool Equals(SummaryAttribute other)
+            => other != null && this.Text.Equals(other.Text, StringComparison.Ordinal);
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+            => this.Equals(obj as SummaryAttribute);
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+            => this.Text.GetHashCode();
     }
 }
