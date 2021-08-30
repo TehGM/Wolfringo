8c2e30aa79280263f883750c293490858b5e289d
TehGM <wojop95@gmail.com>
2021-08-30 17:16:55 +0100

    Added help category attribute

---
+++ Wolfringo.Commands/Attributes/HelpCategoryAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TehGM.Wolfringo.Commands.Attributes
+{
+    /// <summary>Assigns command or all commands in the handler to a specific category in help list.</summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class HelpCategoryAttribute : Attribute
+    {
+        /// <summary>Name of the category.</summary>
+        public string Name { get; }
+        /// <summary>Priority of the category.</summary>
+        public int Priority { get; set; }
+
+        /// <summary>Assigns command or all commands in the handler to a specific category in help list.</summary>
+        /// <param name="name">Name of the category.</param>
+        /// <param name="priority">Priority of the category.</param>
+        public HelpCategoryAttribute(string name, int priority)
+        {
+            this.Name = name;
+            this.Priority = priority;
+        }
+
+        /// <summary>Assigns command or all commands in the handler to a specific category in help list, with priority of 0.</summary>
+        /// <param name="name">Name of the category.</param>
+        public HelpCategoryAttribute(string name)
+            : this(name, 0) { }
+    }
+}
