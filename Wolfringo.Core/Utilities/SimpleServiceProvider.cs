5cf240fcd86bea93989259fdf4e0c75e1a8d433c
TehGM <wojop95@gmail.com>
2021-10-08 12:35:18 +0100

    Moved simple service provider to Core library

--- Wolfringo.Core/Utilities/SimpleServiceProvider.cs
+++ Wolfringo.Core/Utilities/SimpleServiceProvider.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace TehGM.Wolfringo.Commands.Initialization
+namespace TehGM.Wolfringo.Utilities
 {
     /// <summary>A simple service provider.</summary>
-    /// <remarks>This service provider is used automatically by <see cref="CommandsService"/> when no own <see cref="IServiceProvider"/> is provided via constructor injection.</remarks>
+    /// <remarks>This service provider is used automatically by Commands Service when no own <see cref="IServiceProvider"/> is provided via constructor injection.</remarks>
     public class SimpleServiceProvider : IServiceProvider, IServiceScope, IServiceScopeFactory
     {
         /// <inheritdoc/>
@@ -16,7 +16,7 @@ namespace TehGM.Wolfringo.Commands.Initialization
         /// <param name="services">Services mapping.</param>
         public SimpleServiceProvider(IDictionary<Type, object> services)
         {
-            this._services = services ?? new Dictionary<Type, object>();
+            _services = services ?? new Dictionary<Type, object>();
 
             if (!_services.ContainsKey(typeof(IServiceProvider)))
                 _services.Add(typeof(IServiceProvider), this);
@@ -31,7 +31,7 @@ namespace TehGM.Wolfringo.Commands.Initialization
         {
             lock (_services)
             {
-                this._services.TryGetValue(serviceType, out object result);
+                _services.TryGetValue(serviceType, out object result);
                 return result;
             }
         }
