d2b8ca2cc6aec1985a0293834aafb3530eecba30
TehGM <TehGM@users.noreply.github.com>
2024-11-25 11:22:31 +0100

    Add missing doc comments for GroupLinkPosition

--- Wolfringo.Core/Utilities/Internal/GroupLinkPosition.cs
+++ Wolfringo.Core/Utilities/Internal/GroupLinkPosition.cs
@@ -3,10 +3,17 @@
     /// <summary>Represents position of group link in a text.</summary>
     public struct GroupLinkPosition
     {
+        /// <summary>Start of a group link.</summary>
         public int Start { get; }
+        /// <summary>End of a group link.</summary>
         public int End { get; }
+        /// <summary>Contents of group link within brackets (group name).</summary>
         public string Text { get; }
 
+        /// <summary>Create a new instance of group link position.</summary>
+        /// <param name="start">Start of a group link.</param>
+        /// <param name="end">End of a group link.</param>
+        /// <param name="text">Contents of group link within brackets (group name).</param>
         public GroupLinkPosition(int start, int end, string text)
         {
             this.Start = start;
