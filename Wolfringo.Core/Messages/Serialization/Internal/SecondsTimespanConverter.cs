a7bbe70badf6c37d66e6eb166d986fb12dfbc83b
TehGM <TehGM@users.noreply.github.com>
2024-12-09 01:03:22 +0100

    Add SlowModeRate support for chat responses

---
+++ Wolfringo.Core/Messages/Serialization/Internal/SecondsTimespanConverter.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace TehGM.Wolfringo.Messages.Serialization.Internal
+{
+    /// <summary>Json converter for turning minutes into timespan.</summary>
+    public class SecondsTimespanConverter : JsonConverter
+    {
+        /// <inheritdoc/>
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(TimeSpan));
+        }
+
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((TimeSpan)value).TotalSeconds.ToString());
+        }
+
+        /// <inheritdoc/>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.Value is long)
+                return TimeSpan.FromSeconds((long)reader.Value);
+            return TimeSpan.FromSeconds((double)reader.Value);
+        }
+    }
+}
